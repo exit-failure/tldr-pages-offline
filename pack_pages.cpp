b55ca6df0cf6be5a76d50f4477a1e0ba418c7678
Harald Kraft <hakrala@web.de>
2023-09-13 08:55:09 +0200

    added pack_pages

---
+++ pack_pages.cpp
@@ -0,0 +1,18 @@
+#include "pack_pages.h"
+
+void get_pages(string dir) {
+	//IMPL
+}
+
+void add_page(string file) {
+	//IMPL
+}
+
+void write_header() {
+	//IMPL
+}
+
+int main() {
+	//IMPL
+	exit(EXIT_FAILURE);
+}
---
+++ pack_pages.h
@@ -0,0 +1,14 @@
+#ifndef _pack_pages_h_
+#define _pack_pages_h_
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+void get_pages(string dir);
+void add_page(string file);
+void write_header();
+
+#endif //#ifndef _pack_pages.h_
